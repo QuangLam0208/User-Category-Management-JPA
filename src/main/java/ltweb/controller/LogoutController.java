4d2136fcd1141dabc733cdec2a76f2a220166526
Quang Lam <laulua1223@outlook.com>
2025-11-20 20:01:04 +0700

    add logout ctrler

---
+++ src/main/java/ltweb/controller/LogoutController.java
@@ -0,0 +1,48 @@
+package ltweb.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession(false);
+		
+		if (session != null) {
+			session.invalidate();
+		}
+		
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("username")) {
+					cookie.setMaxAge(0); // Set = 0 để xóa cookie
+					cookie.setPath(req.getContextPath() + "/"); 
+					resp.addCookie(cookie);
+					break;
+				}
+			}
+		}
+		
+		resp.sendRedirect(req.getContextPath() + "/login");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+}
