4a3d100e43718ab23b8b3b55db8c0d185d431402
Quang Lam <laulua1223@outlook.com>
2025-11-20 13:06:24 +0700

    use lombok for category

--- src/main/java/ltweb/model/CategoryModel.java
+++ src/main/java/ltweb/model/CategoryModel.java
@@ -1,5 +1,24 @@
 package ltweb.model;
 
-public class CategoryModel {
+import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryModel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	
+	private String name;
+	
+	private String images;
+	
+	private int userid;
+	
 }
