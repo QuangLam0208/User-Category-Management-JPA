23131c81be95105c431b00057f4abd49bb6e31e4
Quang Lam <laulua1223@outlook.com>
2025-11-21 08:09:44 +0700

    1-n video

--- src/main/java/ltweb/entity/Category.java
+++ src/main/java/ltweb/entity/Category.java
@@ -1,16 +1,21 @@
 package ltweb.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -32,4 +37,8 @@ public class Category {
 	@ManyToOne
 	@JoinColumn(name = "userid")
 	private User user;
+	
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+	@ToString.Exclude
+	private List<Video> videos;
 }
